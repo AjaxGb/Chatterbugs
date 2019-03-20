275df396e28575ecdbb766fe91bb5ef2bda10028
AjaxGb <ajaxgb@gmail.com>
2019-03-20 19:13:13 -0400

    Add IPv6 support, in theory

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
 from server import app
-app.run(host='0.0.0.0', port=8000)
+import socket
+
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+sock.bind(('::', 2822))
+
+app.run(sock=sock)
+
+# app.run(
+# 	host='0.0.0.0',
+# 	port=2822, # 0xB06
+# 	)
