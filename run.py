521bfe356e49c6443a00d6de02bd49dc0a40628e
AjaxGb <ajaxgb@gmail.com>
2019-03-27 18:08:24 -0400

    Add IPv6/IPv4 compatibility (?)

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 from server import app
-import socket
 
-sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-sock.bind(('::', 2822))
+port = 2822 # 0xB06
+ipv6 = False
 
-app.run(sock=sock)
+if ipv6:
+	import socket
 
-# app.run(
-# 	host='0.0.0.0',
-# 	port=2822, # 0xB06
-# 	)
+	sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+	sock.bind(('::', port))
+	
+	app.run(sock=sock)
+else:
+	app.run(host='0.0.0.0', port=port)
