961f85a46ac2199efd962ccafba9cff370af00cb
AjaxGb <ajaxgb@gmail.com>
2019-04-26 20:53:13 -0400

    Better UX on lost connection

--- server/static/js/game.js
+++ server/static/js/game.js
@@ -6,6 +6,7 @@ import Box from './box.js';
 import Plant from './plant.js';
 import Word from './word.js';
 import TerrBlock from './terrblock.js';
+import { waitMillis } from './utils.js';
 
 const faceInput = document.getElementById('face-input');
 const connectForm = document.getElementById('connect');
@@ -45,5 +46,50 @@ async function runGame() {
 	const [bgTile] = await engine.loadImages(['ChatterTile'], 'img/', '.png');
 	engine.bgFill = engine.ctx.createPattern(bgTile, 'repeat');
 	
-	await engine.run();
+	try {
+		await engine.run();
+	} catch (e) {
+		if (e.type === 'close') {
+			engine.ctx.textAlign = 'center';
+			engine.ctx.textBaseline = 'middle';
+			
+			
+			engine.ctx.font = '50px monospace';
+			const mainWidth = engine.ctx.measureText('Connection to server lost.').width;
+			engine.ctx.font = '30px monospace';
+			const subWidth = engine.ctx.measureText('Attempting to reconnect...').width;
+			const textWidth = Math.max(mainWidth, subWidth);
+			
+			engine.ctx.fillStyle = '#ddd';
+			engine.ctx.fillRect(
+				(engine.width - textWidth) / 2 - 30, engine.height / 2 - 110,
+				textWidth + 60, 180);
+			
+			engine.ctx.fillStyle = '#000';
+			
+			engine.ctx.font = '50px monospace';
+			engine.ctx.fillText('Connection to server lost.',
+				engine.width / 2, engine.height / 2 - 50, engine.width);
+			
+			engine.ctx.font = '30px monospace';
+			engine.ctx.fillText('Attempting to reconnect...',
+				engine.width / 2, engine.height / 2 + 30, engine.width);
+			
+			await waitMillis(1000);
+			
+			// Wait for server to come back online
+			while (true) {
+				try {
+					const resp = await fetch('', {method: 'HEAD', cache: 'no-store'});
+					if (resp.ok) {
+						console.log(resp);
+						window.location = window.location;
+						break;
+					}
+				} catch (e) {
+					// Do nothing
+				}
+			}
+		}
+	}
 }
