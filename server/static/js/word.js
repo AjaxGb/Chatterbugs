d6297aeb1b660f09fe194668cb1f2cba50d45c99
AjaxGb <ajaxgb@gmail.com>
2019-04-26 21:04:37 -0400

    Fix blurry plant text

--- server/static/js/word.js
+++ server/static/js/word.js
@@ -9,6 +9,8 @@ const netNorms = {
 	text: null,
 };
 
+const textOverscale = 3;
+
 export default class Word extends Entity {
 
 	constructor(id, data) {
@@ -20,21 +22,23 @@ export default class Word extends Entity {
 		
 		if(!this.cachedImg) {
 			this.cachedImg = document.createElement("canvas");
-
-			this.cachedImg.width = (this.text.length)*10;
-			this.cachedImg.height = 20;
 			
-
+			this.width = this.text.length * 10;
+			
+			this.cachedImg.width = this.width * textOverscale;
+			this.cachedImg.height = 20 * textOverscale;
+			
 			const ctx2 = this.cachedImg.getContext("2d");
+			ctx2.scale(textOverscale, textOverscale);
 			ctx2.font = '18px monospace';
 			ctx2.textAlign = 'left';
 			ctx2.textBaseline = 'top';
-			ctx2.fillText(this.text, 0, 0);
+			ctx2.fillText(this.text, 0, 0, this.width);
 		}
 		ctx.save();
 		ctx.translate(...this.pos);
 		ctx.rotate(this.rot);
-		ctx.drawImage(this.cachedImg, -this.cachedImg.width/2, -this.cachedImg.height/2);
+		ctx.drawImage(this.cachedImg, -this.width / 2, -20 / 2, this.width, 20);
 		ctx.restore();
 	}
 }
